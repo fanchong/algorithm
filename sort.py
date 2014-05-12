4a5c798b784f197c7c736b40cc7073845b1e3c77
wormfanfan <jadefanfanchong@gmail.com>
2014-05-12 12:03:14 +0800

    add selection sort

--- sort.py
+++ sort.py
@@ -1,14 +1,30 @@
 '''
-Quick sort and bubble sort
+Quick sort,bubble sort and selection sort
 '''
+import random
 
 
+
+#bubble sort
 def bubble_sort(seq):
-	length = len(seq)
+	length = len(seq) - 1
 	for i in xrange(length):
+		for j in xrange(length-i):
+			if seq[j] >= seq[j+1]:
+				seq[j], seq[j+1] = seq[j+1], seq[j]
+
+
+#selection sort
+
+def select_sort(seq):
+	length = len(seq) - 1
+	for i in xrange(length):
+		minone = i
 		for j in xrange(i, length):
-			if seq[i] >= seq[j]:
-				seq[i], seq[j] = seq[j], seq[i]
+			if seq[j] < seq[minone]:
+				minone = j
+		seq[i], seq[minone] = seq[minone], seq[i]
+
 
 
 #quick sort
@@ -33,10 +49,19 @@ def quick_sort(seq, left, right):
 
 if __name__ == '__main__':
 	print "------quick sort-----"
-	data = [10,39,2,33,4,45]
+	data = [x for x in range(20)]
+	random.shuffle(data)
+	print "before:",data
 	quick_sort(data, 0, len(data)-1)
-	print data
+	print "after:",data
 	print "------bubble sort----"
-	data = [10,39,2,33,4,45]
+	data = [6,5,4,3,2,1]
+	print "before:", data
 	bubble_sort(data)
-	print data
+	print "after:",data
+	print "------selection sort---"
+	data = [x for x in range(10)]
+	random.shuffle(data)
+	print "before:", data
+	select_sort(data)
+	print "after:",data
