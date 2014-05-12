0e99960e1377d5e6d9d6c820ce4dec76ea94c49d
wormfanfan <jadefanfanchong@gmail.com>
2014-05-12 10:54:50 +0800

    add bubble sort

--- sort.py
+++ sort.py
@@ -1,7 +1,17 @@
 '''
-Quick sort
+Quick sort and bubble sort
 '''
 
+
+def bubble_sort(seq):
+	length = len(seq)
+	for i in xrange(length):
+		for j in xrange(i, length):
+			if seq[i] >= seq[j]:
+				seq[i], seq[j] = seq[j], seq[i]
+
+
+#quick sort
 def depart(seq, left, right):
 	flag = seq[left]
 	while left < right:
@@ -15,13 +25,18 @@ def depart(seq, left, right):
 	seq[left] = flag
 	return left
 
-def sort(seq, left, right):
+def quick_sort(seq, left, right):
 	if left < right:
 		middle = depart(seq, left, right)
-		sort(seq, left, middle)
-		sort(seq, middle+1, right)
+		quick_sort(seq, left, middle)
+		quick_sort(seq, middle+1, right)
 
 if __name__ == '__main__':
+	print "------quick sort-----"
 	data = [10,39,2,33,4,45]
-	sort(data, 0, len(data)-1)
+	quick_sort(data, 0, len(data)-1)
+	print data
+	print "------bubble sort----"
+	data = [10,39,2,33,4,45]
+	bubble_sort(data)
 	print data
